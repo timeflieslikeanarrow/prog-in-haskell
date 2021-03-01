656fee0280622c5ca648903e8d20b16da79882cf
Jian Shi <timeflieslikeanarrow001@gmail.com>
2021-03-01 10:00:35 -0500

    added QuickCheck

--- test/Chapter1Spec.hs
+++ test/Chapter1Spec.hs
@@ -1,14 +1,17 @@
-
 module Chapter1Spec where
 
 import Test.Hspec
+import Test.QuickCheck
 
 import Chapter1
+
+prop_double :: Int -> Bool
+prop_double val = double val == 2 * val
 
 spec :: Spec
 spec = do
   describe "double" $ do
-    it "returns double the value when given a number" $
+    it "returns double the value when given a positive number" $
       double 2 `shouldBe` 4
 
     it "returns double the value when given a negative number" $
@@ -16,3 +19,7 @@ spec = do
 
     it "returns double the value when given a floating number" $
        double (2.5) `shouldBe` 5.0  
+
+    it "should be same as 2*x" $ do
+      property prop_double
+       
